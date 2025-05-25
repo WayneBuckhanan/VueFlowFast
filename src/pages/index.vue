5db2c6c9afe8052a9470510bc60f0c962c8ca23c
Wayne Buckhanan <wayne.buckhanan@gmail.com>
2025-05-25 16:56:30 -0400

    Replace Vuestic UI with PrimeVue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,26 +4,32 @@
   See [File based routing | Unplugin Vue Router](https://uvr.esm.is/guide/file-based-routing.html#nested-routes) for more info.
 -->
 <template lang="pug">
-VaLayout(:left="{ absolute: breakpoints.smDown }").h-screen
-  template(#top)
-    VaNavbar(color="primary").py-2
-      template(#left)
-        VaButton(@click="showSidebar = !showSidebar" :icon="showSidebar ? 'menu_open' : 'menu'")
-      template(#center)
-        VaNavbarItem.font-bold.text-lg
-          | {{ title }}
-  template(#left)
-    VaSidebar(v-model="showSidebar")
-      SidebarContents
-  template(#content)
-    main
-      RouterView
+<div class="mb-2 mx-3 flex-1 place-self-end float-right" />
+.flex.flex-col.justify-stretch(class="h-[100vh] bg-slate-100 dark:bg-slate-700")
+  ToolBar
+    //template(#start)
+    template(#center)
+      .text-lg.font-bold {{ title }}
+    //template(#end)
+      Button(variant="outlined") Login
+  .flex.flex-row.flex-1.w-full
+    Panel(
+      toggleable
+      unstyled
+      :pt="{pcToggleButton: {rounded: true, variant: 'outlined', class: 'float-right'}}"
+      class="bg-slate-200 dark:bg-slate-800"
+    ).flex.flex-col.flex-0.w-fit.h-full
+      template(#toggleicon="{collapsed}")
+        i.m-3.mb-2(:class="collapsed ? 'pi pi-angle-double-right' : 'pi pi-angle-double-left'")
+      ScrollPanel.w-fit
+        SidebarContents
+    ScrollPanel.flex.flex-1.w-full.h-full
+      main.w-full
+        RouterView
 </template>
 
 <script setup>
-import { useBreakpoint } from "vuestic-ui"
-const showSidebar = ref(false)
-const breakpoints = useBreakpoint()
+const showSidebar = ref(true)
 
 const route = useRoute()
 const title = route?.meta?.['title'] || 'Default Header Text'
