dac5d57a3f600b6827a7585956cfccad8e1d4156
Wayne Buckhanan <wayne.buckhanan@gmail.com>
2025-06-19 12:51:45 -0400

    Fix flexboxes in main layout

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,25 +5,38 @@
 -->
 <template lang="pug">
 <div class="mb-2 mx-3 flex-1 place-self-end float-right" />
-.flex.flex-col.justify-stretch(class="h-[100vh] bg-slate-100 dark:bg-slate-700")
+.flex.flex-col.min-h-screen(class="bg-slate-100 dark:bg-slate-700")
   ToolBar
     //template(#start)
     template(#center)
       .text-lg.font-bold {{ title }}
     //template(#end)
       Button(variant="outlined") Login
-  .flex.flex-row.flex-1.w-full
-    Panel(
-      toggleable
-      unstyled
-      :pt="{pcToggleButton: {rounded: true, variant: 'outlined', class: 'float-right'}}"
-      class="bg-slate-200 dark:bg-slate-800"
-    ).flex.flex-col.flex-0.w-fit.h-full
-      template(#toggleicon="{collapsed}")
-        i.m-3.mb-2(:class="collapsed ? 'pi pi-angle-double-right' : 'pi pi-angle-double-left'")
-      ScrollPanel.w-fit
-        SidebarContents
-    ScrollPanel.flex.flex-1.w-full.h-full
+
+  .flex.flex-row.flex-grow.w-full
+    // Sidebar Container
+    .flex.flex-col.w-fit.p-2(
+      class="bg-slate-200 dark:bg-slate-800 transition-all duration-300 ease-in-out"
+    )
+      .flex.flex-col.gap-2.sticky.top-2
+        // Sidebar Toggle Button
+        Button.place-self-end(
+          @click="showSidebar = !showSidebar"
+          variant="outlined"
+          severity="secondary"
+          :pt="{root: {class: 'p-1 !rounded-full'}}"
+        )
+          i.pi.pi-angle-double-left(v-if="showSidebar")
+          i.pi.pi-angle-double-right(v-else)
+        // Sidebar Contents
+        ScrollPanel(
+          v-show="showSidebar"
+          class="bg-slate-200 dark:bg-slate-800 transition-all duration-300 ease-in-out"
+        )
+          SidebarContents
+
+    // Main Contents
+    ScrollPanel.flex.w-full
       main.w-full
         RouterView
 </template>
